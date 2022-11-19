d0efe030db19d782997573072bd57cda37fa9372
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-19 23:14:17 +0900

    [ADD] PageControl

---
+++ PageControl/PageControl/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  PageControl
+//
+//  Created by 홍승현 on 2022/11/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+}
+
---
+++ PageControl/PageControl/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by 홍승현 on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
