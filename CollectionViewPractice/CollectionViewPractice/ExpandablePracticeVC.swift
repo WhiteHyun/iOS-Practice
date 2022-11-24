0e29c8fba57a5efe5dd786d2268654735557c594
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-24 11:12:54 +0900

    [CHORE] Expandable in CollectionView Practice

--- CollectionViewPractice/CollectionViewPractice/ExpandablePracticeVC.swift
+++ CollectionViewPractice/CollectionViewPractice/ExpandablePracticeVC.swift
@@ -13,13 +13,12 @@ import Then
 final class ExpandablePracticeVC: UIViewController {
   
   private lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
-    let layout = UICollectionViewCompositionalLayout.list(using: .init(appearance: .insetGrouped))
-    $0.collectionViewLayout = layout
+    $0.collectionViewLayout = createLayout()
     $0.backgroundColor = .systemYellow
     $0.dataSource = self
     $0.register(ExpandableCollectionViewCell.self, forCellWithReuseIdentifier: ExpandableCollectionViewCell.id)
   }
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
@@ -28,6 +27,34 @@ final class ExpandablePracticeVC: UIViewController {
     collectionView.snp.makeConstraints { make in
       make.edges.equalTo(self.view.safeAreaLayoutGuide)
     }
+  }
+    
+  private func createLayout() -> UICollectionViewCompositionalLayout {
+    
+    // item
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                          heightDimension: .fractionalHeight(1))
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+    // group
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: .init(widthDimension: .absolute(70),
+                        heightDimension: .absolute(70)),
+      subitems: [item]
+    )
+
+    // section
+    let section = NSCollectionLayoutSection(group: group)
+    section.orthogonalScrollingBehavior = .continuous
+    section.interGroupSpacing = 10
+    section.contentInsets = .init(top: 0, leading: 10, bottom: 30, trailing: 10)
+    section.boundarySupplementaryItems = [
+      .init(layoutSize: .init(widthDimension: .fractionalWidth(1),
+                              heightDimension: .estimated(50)),
+            elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+    ]
+    
+    return UICollectionViewCompositionalLayout(section: section)
   }
 }
 
@@ -41,8 +68,19 @@ extension ExpandablePracticeVC: UICollectionViewDataSource {
     guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExpandableCollectionViewCell.id, for: indexPath) as? ExpandableCollectionViewCell else {
       fatalError()
     }
-    cell.backgroundColor = .link
+    cell.backgroundColor = .systemGray5
+    cell.layer.cornerRadius = cell.frame.height / 2
     cell.configure(with: String(indexPath.item))
     return cell
   }
 }
+
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct ExpandablePracticeVCPreview: PreviewProvider {
+  static var previews: some View {
+    ExpandablePracticeVC().toPreview()
+  }
+}
+#endif
