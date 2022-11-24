e6c58fce3a53c17f711c1d32aa4cb4f8a5024b45
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-24 11:30:42 +0900

    [FEAT] Simple present logic in presentController practice

---
+++ PresentationController/PresentationController/SheetViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SheetViewController.swift
+//  PresentationController
+//
+//  Created by 홍승현 on 2022/11/24.
+//
+
+import UIKit
+
+final class SheetViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemCyan
+  }
+}
--- PresentationController/PresentationController/ViewController.swift
+++ PresentationController/PresentationController/ViewController.swift
@@ -12,7 +12,38 @@ import Then
 
 final class ViewController: UIViewController {
   
+  private lazy var button = UIButton(
+    configuration: .tinted(),
+    primaryAction: .init(handler: { _ in
+      let nextVC = SheetViewController()
+      self.present(nextVC, animated: true)
+    })
+  )
+    .then {
+      $0.setTitle("Present Sheet", for: .normal)
+      $0.configuration?.cornerStyle = .capsule
+    }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+  
+  private func setupLayouts() {
+    view.addSubview(button)
+  }
+  
+  private func setupConstraints() {
+    button.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+      make.width.equalTo(200)
+      make.height.equalTo(45)
+    }
+  }
+  
+  private func setupStyles() {
+    view.backgroundColor = .systemBackground
   }
 }
