e951ee1c3ed76d3acb6b7e88840c253cfe0911a4
SeungHyun Hong <whi7ehyun@gmail.com>
2023-07-04 23:25:17 +0900

    [ADD] Unit Tests in SwiftUIPractice project with yaml workflows

---
+++ SwiftUIPractice/SwiftUIPracticeTests/SwiftUIPracticeTests.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIPracticeTests.swift
+//  SwiftUIPracticeTests
+//
+//  Created by 홍승현 on 2023/07/04.
+//
+
+import XCTest
+
+final class SwiftUIPracticeTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testAssertionTrue() throws {
+    XCTAssertTrue(true)
+  }
+}
